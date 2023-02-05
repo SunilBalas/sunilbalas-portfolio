83c2cba92f84d969e9c61fd0a1e4c703acc5e3c3
SunilBalas <balassunil606@gmail.com>
2023-02-05 20:02:17 +0530

    added code for responsive portfolio website
    added gitignore file
    added favicon icons
    improved the quality of profile image
    seperated the js code in different file
    added the Download Resume feature

---
+++ js/portfolio.js
@@ -0,0 +1,77 @@
+// lazy loading
+AOS.init({
+    duration: 1000
+});
+
+// configuring the year in footer
+var year = document.getElementById("year");
+year.innerText = new Date().getFullYear();
+year.style.color = "#ffffff";
+
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+});
+
+// download resume
+function downloadResume(fileName) {
+    var url = "../resume/" + fileName + ".pdf";
+
+    // Create XMLHTTP Request.
+    var req = new XMLHttpRequest();
+    req.open("GET", url, true);
+    req.responseType = "blob";
+    req.onload = function () {
+        // Convert the Byte Data to BLOB object.
+        var blob = new Blob([req.response], { type: "application/pdf" });
+
+        // Check the Browser type and download the File.
+        var isIE = false || !!document.documentMode;
+        if (isIE) {
+            window.navigator.msSaveBlob(blob, fileName);
+        } else {
+            var url = window.URL || window.webkitURL;
+            link = url.createObjectURL(blob);
+            var a = document.createElement("a");
+            a.setAttribute("download", fileName);
+            a.setAttribute("href", link);
+            document.body.appendChild(a);
+            a.link = "Resume-Sunil_Balas.pdf"
+            a.click();
+            document.body.removeChild(a);
+        }
+    };
+    req.send();
+};
